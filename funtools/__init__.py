9a0079b3f57ebfb141e83c70180eb1b244cfae4e
Alan Mbe <alanmbe2013@gmail.com>
2025-04-13 18:24:54 +0800

    Update __init__.py

--- funtools/__init__.py
+++ funtools/__init__.py
@@ -3,3 +3,4 @@ from .gametools import *    # Simple game creation tools
 from .guitools import *     # Fast GUI (Graphical User Interface) setup tools
 from .captchatools import * # CAPTCHA (verification image) generation tools
 from .stattools import *    # Tools for generating statistical charts
+from .imagetools import * 
